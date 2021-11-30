680780604404fc8ee4cc0f24dab61502a87a3aa8
unknown <ayushsaraf10@gmail.com>
2021-11-30 16:42:16 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 import React from "react";
-import Dashboard from "./Component/Dashboard/Dashboard";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import AssignedWork from "./Component/AssignedWork/AssignedWork";
-import PopUp from "./Component/PopUp/PopUp";
+import Home from "./Component/Home";
 
 const App = () => {
   return (
     <div>
-      <Dashboard />
-      <BrowserRouter>
-        <Routes>
-          <Route exact path="/" component={Dashboard} />
-          <Route exact path="/assignedwork" component={AssignedWork} />
-          <Route exact path="/popup" component={PopUp} />
-        </Routes>
-      </BrowserRouter>
+      <Home />
     </div>
   );
 };
